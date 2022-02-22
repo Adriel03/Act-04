69466fe6d79cd0ccb49a0bc721c2f21ae9534925
Adriel03 <99216506+Adriel03@users.noreply.github.com>
2022-02-21 19:52:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
 import './style.css';
-import * as THREE from '/three';
-import { OrbitControls } from '/three/examples/jsm/controls/OrbitControls';
+import * as THREE from './three';
+import { OrbitControls } from './three/examples/jsm/controls/OrbitControls';
 
 // Setup
 
